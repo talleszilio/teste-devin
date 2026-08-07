5fe2727171d9b5559ba3e27982c305cf1c6459c9
talleszilio <talleszilio@gmail.com>
2026-08-07 17:28:51 -0300

    test: adicionar testes unitários em Python e JavaScript
    - Adicionados testes unitários Python em tests/example_test.py
    - Adicionados utilitários JavaScript em example-project/src/utils.js
    - Adicionados testes JavaScript em example-project/tests/utils.test.js
    - Testes cobrem cenários comuns de desenvolvimento

---
+++ example-project/tests/utils.test.js
@@ -0,0 +1,60 @@
+/**
+ * Testes para utilitários
+ * Simula testes unitários em JavaScript
+ */
+
+const {
+    toTitleCase,
+    validateEmail,
+    generateId,
+    formatDateBR
+} = require('../src/utils');
+
+describe('Utils', () => {
+    describe('toTitleCase', () => {
+        test('deve converter primeira letra para maiúscula', () => {
+            expect(toTitleCase('talles')).toBe('Talles');
+        });
+        
+        test('deve manter demais letras minúsculas', () => {
+            expect(toTitleCase('TALLES')).toBe('Talles');
+        });
+        
+        test('deve retornar string vazia para input vazio', () => {
+            expect(toTitleCase('')).toBe('');
+        });
+    });
+    
+    describe('validateEmail', () => {
+        test('deve validar email correto', () => {
+            expect(validateEmail('talleszilio@gmail.com')).toBe(true);
+        });
+        
+        test('deve rejeitar email sem @', () => {
+            expect(validateEmail('tallesziliogmail.com')).toBe(false);
+        });
+        
+        test('deve rejeitar email sem domínio', () => {
+            expect(validateEmail('talleszilio@')).toBe(false);
+        });
+    });
+    
+    describe('generateId', () => {
+        test('deve gerar ID único', () => {
+            const id1 = generateId();
+            const id2 = generateId();
+            expect(id1).not.toBe(id2);
+        });
+        
+        test('deve retornar string', () => {
+            expect(typeof generateId()).toBe('string');
+        });
+    });
+    
+    describe('formatDateBR', () => {
+        test('deve formatar data corretamente', () => {
+            const date = new Date('2026-08-07');
+            expect(formatDateBR(date)).toBe('07/08/2026');
+        });
+    });
+});
